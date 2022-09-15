00a25b852a26dfd4cd67b18629166f0fa3c4342c
TossoTosca <nugrohosantoso454545@gmail.com>
2022-09-15 21:47:16 +0700

    push ejs sebelum pull server

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 
 router.get("/", userController.userList);
+// rouer.get("/addCourse", userController.addForm)
 // router.get("/", userController.default);
 // router.post("/", userController.default);
 // router.get("/", userController.default);

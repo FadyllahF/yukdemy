7f9f6dd1762d01f600158ea296fe7c3bbe274c03
FadyllahF <farhanfady1@gmail.com>
2022-09-15 15:57:22 +0700

    login dan register

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,45 @@
 const sequelize = require("sequelize");
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 class Controller {
   static home(req, res) {
     res.render("homePage");
+  }
+  static registerForm(req, res) {
+    res.render("registerForm");
+  }
+  static postRegister(req, res) {
+    const { userName, password, role } = req.body;
+    User.create({ userName, password, role })
+      .then((newUser) => {
+        res.redirect("/loginPage");
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+  static loginForm(req, res) {
+    res.render("loginPage");
+  }
+  static postLogin(req, res) {
+    const { userName, password } = req.body;
+    User.findOne({ where: { userName } })
+      .then((user) => {
+        //console.log(user);
+        if (user) {
+          const isValidPassword = bcrypt.compareSync(password, user.password);
+          if (isValidPassword) {
+            return res.redirect("/");
+          } else {
+            const error = "invalid username or password ";
+            return res.redirect(`loginPage?error=${error}`);
+          }
+        }
+      })
+      .catch((err) => {
+        res.send(err);
+      });
   }
 }
 
